69ccbd964b060dd5adc841fe4c668431caef90f3
Vikash Gupta <137061206+vik802207@users.noreply.github.com>
2025-05-31 12:03:07 +0530

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -13,7 +13,7 @@ const Dashboard = () => {
       padding: "2rem"
     },
     homeCard: {
-      background: "rgba(255, 255, 255, 0.08)",
+      background: "#030202",
       borderRadius: "20px",
       padding: "3rem",
       textAlign: "center",
